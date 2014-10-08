a2e310ef55872d8a8569d39f6b33660ab95167a4
Gabriel Rodrigues Dias <admsystem@worldtoth.com.br>
2014-10-08 02:36:07 -0300

    adicionei estrutura do site

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+	<html lang="pt-br">
+		<head>
+			<meta charset="utf-8">
+			<title>Casa Amiga | Imoveis e Administração</title>
+			<meta name="viewport" content="width=device-width, initial-scale=1">
+			<meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1">
+			<meta name="ROBOTS" content="INDEX,FOLLOW">
+			<meta name="description" content="Encontre aqui seu imovel">
+			<meta name="keywords" content="Imovel, Casa, Aluguel de casa, Imobiliaria">
+			<meta name="author" content="WorldToth - WebDeveloper">
+				<!-- Styles -->
+				<link rel="stylesheet" href="css/style.css">
+				<!-- Scripts-->
+				<script src="js/moderniz.js"></script>
+		</head>
+		
+		<body>
+			
+		</body>
+		<script src="jquery-1.11.1.min.js"></script>
+	</html>
