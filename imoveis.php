0229e9f124cfad0079c1ce1b8b32c8a02a3de313
Gabriel Rodrigues Dias <admsystem@worldtoth.com.br>
2014-10-20 18:40:40 -0200

    arrumei a estrutura da pagina imoveis

--- imoveis.php
+++ imoveis.php
@@ -18,56 +18,56 @@
 		</head>
 		
 		<body>
-		<div class="container">
-			<header>
-				<div class="header-fit">
-					<div class="header-content">
-						<h1>
-							<img src="img/logotipo.png" alt="logoimage">
-						CASA AMIGA
-							<p>consultoria imobili&aacute;ria</p>
-						</h1>
-						<nav>
+			<div class="container">
+				<header>
+					<div class="header-fit">
+						<div class="header-content">
+							<h1>
+								<img src="img/logotipo.png" alt="logoimage">
+							CASA AMIGA
+								<p>consultoria imobili&aacute;ria</p>
+							</h1>
+							<nav>
+								<ul>
+									<li><a class="icon icon-casa" href="#"><span>Home</span></a></li>
+									<li><a class="icon icon-chave" href="#"><span>Im&oacute;veis</span></a></li>
+									<li><a class="icon icon-image" href="#"><span>Loteamento</span></a></li>
+									<li><a class="icon icon-lancamento" href="#"><span>Lan&ccedil;amentos</span></a></li>
+									<li><a class="icon icon-chat" style="margin-right: 0;" href="#"><span>Contato</span></a></li>
+								</ul>
+							</nav>
+						</div>
+					</div>
+				</header>
+					<footer>
+					<div class="footer-fit">
+						<div class="footer-fit-ul">
 							<ul>
-								<li><a class="icon icon-casa" href="#"><span>Home</span></a></li>
-								<li><a class="icon icon-chave" href="#"><span>Im&oacute;veis</span></a></li>
-								<li><a class="icon icon-image" href="#"><span>Loteamento</span></a></li>
-								<li><a class="icon icon-lancamento" href="#"><span>Lan&ccedil;amentos</span></a></li>
-								<li><a class="icon icon-chat" style="margin-right: 0;" href="#"><span>Contato</span></a></li>
+								<li><a class="icon icon-casa" href=""><span>Casa</span></a></li>
+								<li><a class="icon icon-apartamento" href=""><span>Apartamento</span></a></li>
+								<li><a class="icon icon-image" href=""><span>Loteamento</span></a></li>
+								<li><a class="icon icon-home" href=""><span>Comercial</span></a></li>
 							</ul>
-						</nav>
+							<ul>
+								<li><a class="" href=""><span>Contato</span></a></li>
+								<li><a class="" href=""><span>Tel: (12)0000-0000</span></a></li>
+								<li><a class="" href=""><span>Email: exemple@exemple.com</span></a></li>
+								<li><a class="" href=""><span>Skype: </span></a></li>
+							</ul>
+							<ul>
+								<li><a class="" href=""><span>Siga-nos</span></a></li>
+								<li><a class="icon icon-facebook" href=""><span>Facebook</span></a></li>
+								<li><a class="" href=""><span>Skype</span></a></li>
+								<li><a class="" href=""><span>Twitter</span></a></li>
+							</ul>
+						</div>
+						<div class="newslleter">
+							<input name="newslleter" type="email">
+							<label>Newslleter</label>
+						</div>
 					</div>
-				</div>
-			</header>
-				<footer>
-				<div class="footer-fit">
-					<div class="footer-fit-ul">
-						<ul>
-							<li><a class="icon icon-casa" href=""><span>Casa</span></a></li>
-							<li><a class="icon icon-apartamento" href=""><span>Apartamento</span></a></li>
-							<li><a class="icon icon-image" href=""><span>Loteamento</span></a></li>
-							<li><a class="icon icon-home" href=""><span>Comercial</span></a></li>
-						</ul>
-						<ul>
-							<li><a class="" href=""><span>Contato</span></a></li>
-							<li><a class="" href=""><span>Tel: (12)0000-0000</span></a></li>
-							<li><a class="" href=""><span>Email: exemple@exemple.com</span></a></li>
-							<li><a class="" href=""><span>Skype: </span></a></li>
-						</ul>
-						<ul>
-							<li><a class="" href=""><span>Siga-nos</span></a></li>
-							<li><a class="icon icon-facebook" href=""><span>Facebook</span></a></li>
-							<li><a class="" href=""><span>Skype</span></a></li>
-							<li><a class="" href=""><span>Twitter</span></a></li>
-						</ul>
-					</div>
-					<div class="newslleter">
-						<input name="newslleter" type="email">
-						<label>Newslleter</label>
-					</div>
-				</div>
-			</footer>
-		</div>
-				</body>
+				</footer>
+			</div>
+		</body>
 		
 	</html>
